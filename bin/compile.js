7d36367faec1afa68c9a33144551975b71ba4792
BJR Matos <bjrmatos@gmail.com>
2017-03-06 17:35:45 -0500

    default output name on UNIX should be “jsreport”

--- bin/compile.js
+++ bin/compile.js
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
+
+var defaultOutput = 'jsreport'
+
+if (process.platform === 'win32') {
+  defaultOutput = 'jsreport.exe'
+}
+
 var argv = require('yargs')
     .usage('Usage: $0 -i [server.js] -o [jsreport.exe]')
     .options('i', {
@@ -9,7 +16,7 @@ var argv = require('yargs')
     }).options('o', {
       alias: 'output',
       desc: 'The output binary or bundle file',
-      default: 'jsreport.exe'
+      default: defaultOutput
     }).options('b', {
       alias: 'bundle',
       default: false,
@@ -17,12 +24,12 @@ var argv = require('yargs')
     })
     .help()
     .example('$0')
-    .example('$0 -i startup.js -o jsreport.exe')
+    .example('$0 -i startup.js -o ' + defaultOutput)
     .example('$0 -b -i startup.js -o jsreport.js')
     .example('$0 -b')
     .argv
 
-if (argv.bundle && argv.output === 'jsreport.exe') {
+if (argv.bundle && argv.output === defaultOutput) {
   argv.output = 'jsreport.js'
 }
 
